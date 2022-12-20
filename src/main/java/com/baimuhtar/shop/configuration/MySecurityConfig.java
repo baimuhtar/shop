bcd30f600d82bd672224feb18afe395b6d9064d5
baimuhtar <baimuhtar1997@gmail.com>
2022-12-20 14:54:17 +0600

    Creating configure method for roles

---
+++ src/main/java/com/baimuhtar/shop/configuration/MySecurityConfig.java
@@ -0,0 +1,37 @@
+package com.baimuhtar.shop.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+
+        builder.jdbcAuthentication().dataSource(dataSource);
+    }
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/").hasAnyRole("SELLER","BUYER")
+                .antMatchers(HttpMethod.GET, "/products").hasAnyRole("SELLER", "BUYER")
+                .antMatchers(HttpMethod.GET, "/products/{id}").hasAnyRole("SELLER", "BUYER")
+                .antMatchers(HttpMethod.POST, "/products").hasRole("SELLER")
+                .antMatchers(HttpMethod.PUT, "/products").hasRole("SELLER")
+                .antMatchers(HttpMethod.DELETE, "/products").hasRole("SELLER")
+                .and().formLogin().permitAll();
+    }
+}
